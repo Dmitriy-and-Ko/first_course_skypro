b9ff8104c5f71e338fe8d7e6a1588d89bf71815a
Dmitriy Kovshov <dmitriykovshov32@gmail.com>
2025-01-05 19:01:16 +0300

    utils created

--- src/reports.py
+++ src/reports.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 from typing import Optional
 from pathlib import Path
@@ -5,6 +7,7 @@ import csv
 from datetime import date
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+from numpy.ma.core import shape
 
 PATH_TO_DIR = Path(__file__).parent.parent
 PATH_TO_FILE = Path(PATH_TO_DIR, 'data', 'operations.xlsx')
@@ -28,6 +31,7 @@ def excel_reader(excel_path: str) ->pd.DataFrame:
     try:
         if excel_path == PATH_TO_FILE:
             excel_data = pd.read_excel(PATH_TO_FILE, sheet_name="Отчет по операциям" )
+
             shape_data = excel_data.shape
             list_of_dicts = [dict(excel_data.iloc[x]) for x in range(shape_data[0])]
             return list_of_dicts
@@ -102,9 +106,76 @@ def csv_to_iterator(file_path):
 
 
 if __name__ == '__main__':
-    for row in csv_to_iterator(PATH_TO_FILE_CSV):
-        print(row)
-    # print(excel_reader(PATH_TO_FILE))
+    # for row in csv_to_iterator(PATH_TO_FILE_CSV):
+    #     print(row)
+    # df_with_index_category = pd.read_excel(PATH_TO_FILE, sheet_name="Отчет по операциям", index_col=9)
+    date = '2020-03-18 9:43:51'
+    date_format = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').strftime('%d.%m.%Y %H:%M:%S')
+    obj_date_upper = datetime.strptime(date_format, '%d.%m.%Y %H:%M:%S')
+    year_upper_date = obj_date_upper.year
+    month_upper_date = obj_date_upper.month
+    day_upper_date = obj_date_upper.day
+    hour_upper_date = obj_date_upper.hour
+    minute_upper_date = obj_date_upper.minute
+    second_upper_date = obj_date_upper.second
+    obj_date_lower = datetime(year_upper_date, month_upper_date, 1, hour_upper_date, minute_upper_date,
+                                       second_upper_date)
+    str_date_lower = datetime.strftime(obj_date_lower, '%d.%m.%Y %H:%M:%S')
+    str_date_upper = datetime.strftime(obj_date_upper, '%d.%m.%Y %H:%M:%S')
+    print([str_date_lower, str_date_upper])
+
+    print(obj_date_lower)
+    print(type(obj_date_lower))
+    print(obj_date_upper)
+    print(type(obj_date_upper))
+
+
+    df = pd.read_excel(PATH_TO_FILE, sheet_name="Отчет по операциям")
+
+
+
+    df_category = df.groupby('Категория')
+    print(df_category['Сумма платежа'].mean())
+    group_df_category = df_category['Сумма платежа'].mean()
+    list_dict = group_df_category.to_dict()
+    print(list_dict)
+
+    # df_with_index_card_number = pd.read_excel(PATH_TO_FILE, sheet_name="Отчет по операциям", index_col=2)
+    # group_df_index_card_number = df_with_index_card_number['Сумма платежа'].mean()
+    # print(group_df_index_card_number)
+
+
+    # df_group_card_number_sum_pay_cash_back = df.groupby(['Номер карты', 'Сумма платежа', 'Кэшбэк']).sum()
+    # print(df_group_card_number_sum_pay_cash_back)
+
+
+
+    df_card_number = df.groupby('Номер карты')
+    df_card_number_sum_pay = df_card_number.agg({'Сумма платежа': 'mean', 'Кэшбэк': 'mean'})
+    shape_card_number_sum_pay = df_card_number_sum_pay.shape
+    list_dict_card_number = []
+    # for i in range(shape_card_number_sum_pay[0]):
+    #     list_dict_card_number.append(df_card_number_sum_pay.iloc[i].to_dict())
+    # print(list_dict_card_number)
+    # for i in range(shape_card_number_sum_pay[0]):
+    #     list_dict_card_number.append
+    #     {df_card_number_sum_pay.iloc[i, 0]: df_card_number_sum_pay.iloc[i, 1]}
+    # print(list_dict_card_number)
+
+    # for i in range(shape_card_number_sum_pay[0]):
+    #     list_dict_card_number.append
+    #     {df_card_number('Номер карты')}
+    # print(list_dict_card_number)
+
+    # print(df_card_number_sum_pay)
+    # print(shape_card_number_sum_pay)
+    # list_card_dict = df_card_number_sum_pay.to_dict()
+    # print(list_card_dict)
+
+
+
+
+
     # csv_reader(PATH_TO_FILE_CSV)
     # print(spending_by_category([], 'категория', '01.01.2018 11:34:02'))
     # file_name = PATH_TO_FILE_CSV
